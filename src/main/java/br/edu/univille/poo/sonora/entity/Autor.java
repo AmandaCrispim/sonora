5e94ddeb4b4125048b9e00141abce212d6a1225a
AmandaCrispim <amandacrispim384@gmail.com>
2024-09-28 17:27:25 -0300

    feat: criação da tabela Autor

--- src/main/java/br/edu/univille/poo/sonora/entity/Autor.java
+++ src/main/java/br/edu/univille/poo/sonora/entity/Autor.java
@@ -3,18 +3,12 @@ package br.edu.univille.poo.sonora.entity;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
-import jakarta.persistence.ManyToOne;
 
 @Entity
-public class Musica {
+public class Autor {
 
     @Id
     @GeneratedValue
-    private long id;
-
-    private String titulo;
-    private String autor;
-    private String genero;
-    private double duracao;
-
+    private Long id;
+    private String nome;
 }
---
+++ src/main/java/br/edu/univille/poo/sonora/entity/Playlist.java
@@ -0,0 +1,4 @@
+package br.edu.univille.poo.sonora.entity;
+
+public class Playlist {
+}
