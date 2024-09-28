60c5eb302be7730e1293488b41b8237f5b48080e
AmandaCrispim <amandacrispim384@gmail.com>
2024-09-28 14:00:32 -0300

    feat: criação do projeto

---
+++ src/main/java/br/edu/univille/poo/sonora/SonoraApplication.java
@@ -0,0 +1,13 @@
+package br.edu.univille.poo.sonora;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SonoraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SonoraApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/edu/univille/poo/sonora/SonoraApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.univille.poo.sonora;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SonoraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
